7e7540ad5f22ba369bbf39b1c1f0fffe004640f9
Renata Pedroso <renatanicollyy1@gmail.com>
2021-06-25 11:18:07 -0300

    conexão com banco de dados

--- src/br/com/tuma/jdbc/Conexao.java
+++ src/br/com/tuma/jdbc/Conexao.java
@@ -1,5 +1,24 @@
 package br.com.tuma.jdbc;
 
-public class Conexao {
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+public class Conexao {
+	
+	public static Connection obterConexao() {
+		
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost/dbalunos?useSSL=false", "root", "root");
+			System.out.println("Banco de dados conectado com sucesso!!");
+		} catch (SQLException e) {
+			System.err.println("Não foi possível conectar ao banco de dados:(");
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
+	
 }
